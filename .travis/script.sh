1e389488f12228451e106f275a54bb364b92a671
Warfork <51063252+Warfork@users.noreply.github.com>
2020-06-21 01:35:10 -0400

    General CI improvements
    https://github.com/mmatyas/pegasus-frontend/commit/0cf4d38afb1358e0f03d67ddb9222d74e23b38d5

--- .travis/script.sh
+++ .travis/script.sh
@@ -44,7 +44,8 @@ ${QT_HOSTDIR}/bin/qmake .. \
   INSTALL_DESKTOPDIR=/usr/share/applications \
   INSTALL_APPSTREAMDIR=/usr/share/metainfo \
   INSTALL_DOCDIR=/usr/share/doc/fvi-launcher \
-  QMAKE_CXXFLAGS+=-fno-rtti
+   QMAKE_CXXFLAGS+='-fno-rtti -static-libstdc++ -static-libgcc' \
+  QMAKE_LFLAGS+='-fno-rtti -static-libstdc++ -static-libgcc'
 make
 make install INSTALL_ROOT=$PWD/../installoc
 
