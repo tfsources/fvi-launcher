18be092ab292e1b327bdcbb49c7710e88fcac2be
Warfork <51063252+Warfork@users.noreply.github.com>
2021-06-24 16:02:46 -0400

    Removed the manual no-rtti flags from the CI config

--- .travis/script.sh
+++ .travis/script.sh
@@ -44,8 +44,7 @@ ${QT_HOSTDIR}/bin/qmake .. \
   INSTALL_DESKTOPDIR=/usr/share/applications \
   INSTALL_APPSTREAMDIR=/usr/share/metainfo \
   INSTALL_DOCDIR=/usr/share/doc/fvi-launcher \
-  QMAKE_CXXFLAGS+='-fno-rtti' \
-  QMAKE_LFLAGS+='-fno-rtti'
+
 make
 make install INSTALL_ROOT=$PWD/../installoc
 
